ab637901ad8c624b1ec36d32f4ac7c91ad62ccd4
TarunNagarajan <tarun.greenville@gmail.com>
2025-11-20 20:26:52 +0530

    chore: directory cleanup for restructuring and project change

--- run_model_inspection.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-REM Ensure we are in the project root directory
-pushd "%~dp0"
-build\Release\model-inspection.exe llama\llama.cpp\build\bin\Release\tinyllama.gguf
-popd
-
---
+++ src/gguf-inspection.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <memory>
+#include <iomanip>
+#include <vector>
+#include <string>
+
+#include "ggml.h"
+#include "gguf.h"
+
+
