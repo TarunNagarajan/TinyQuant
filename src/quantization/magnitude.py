88d83a6500720b5210dc3588223b679142f80d4d
TarunNagarajan <tarun.greenville@gmail.com>
2025-11-24 18:07:29 +0530

    feat: add L1 norm for weights calculation as a pruning metric

---
+++ src/quantization/magnitude.py
@@ -0,0 +1,14 @@
+import torch
+
+# calculate the L1 norm of the weights, as a standard magnitude pruning metric
+def compute_magnitude_info(model):
+    sensitivity_map = {}
+    print("[COMPUTING L1 SCORES OF WEIGHTS]")
+
+    with torch.no_grad():
+        for name, param in model.named_parameters():
+            if name == "weight":
+                sensitivity_map[name] = param.abs().sum().item()
+
+    return sensitivity_map
+
